94910ae014dc5c4aaa4d03efcf50e6c30a9d8cb1
loc <huuloc.phan@gmail.com>
2016-11-08 10:33:01 +0700

    change depedency

--- main.go
+++ main.go
@@ -4,24 +4,40 @@ import (
     "fmt"
     "net/http"
 
+    "github.com/russross/blackfriday"
     "github.com/julienschmidt/httprouter"
+    "os"
 )
 
 func main() {
-    r := httprouter.New()
-    r.GET("/", HomeHandler)
+    port := os.Getenv("PORT")
+    if port == "" {
+        port = "8080"
+    }
 
-    // Posts collection
-    r.GET("/posts", PostsIndexHandler)
-    r.POST("/posts", PostsCreateHandler)
+    http.HandleFunc("/markdown", GenerateMarkdown)
+    http.Handle("/", http.FileServer(http.Dir("public")))
+    http.ListenAndServe(":"+port, nil)
 
-    // Posts singular
-    r.GET("/posts/:id", PostShowHandler)
-    r.PUT("/posts/:id", PostUpdateHandler)
-    r.GET("/posts/:id/edit", PostEditHandler)
+    //r := httprouter.New()
+    //r.GET("/", HomeHandler)
+    //
+    //// Posts collection
+    //r.GET("/posts", PostsIndexHandler)
+    //r.POST("/posts", PostsCreateHandler)
+    //
+    //// Posts singular
+    //r.GET("/posts/:id", PostShowHandler)
+    //r.PUT("/posts/:id", PostUpdateHandler)
+    //r.GET("/posts/:id/edit", PostEditHandler)
+    //
+    //fmt.Println("Starting server on :8080")
+    //http.ListenAndServe(":8080", r)
+}
 
-    fmt.Println("Starting server on :8080")
-    http.ListenAndServe(":8080", r)
+func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
+    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+    rw.Write(markdown)
 }
 
 func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
