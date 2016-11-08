22e26baa0da07e55d60c86f4d5ea141fe5697607
loc <huuloc.phan@gmail.com>
2016-11-08 11:15:27 +0700

    change depedency

--- main.go
+++ main.go
@@ -1,73 +1,73 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "github.com/russross/blackfriday"
-    "github.com/julienschmidt/httprouter"
+	"github.com/russross/blackfriday"
+	"github.com/julienschmidt/httprouter"
 
-    //"os"
-    "os"
+	//"os"
+	"os"
 )
 
 func main() {
-    port := os.Getenv("PORT")
-    if port == "" {
-        port = "8080"
-    }
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
 
-    //http.HandleFunc("/markdown", GenerateMarkdown)
-    //http.Handle("/", http.FileServer(http.Dir("public")))
-    //http.ListenAndServe(":"+port, nil)
+	//http.HandleFunc("/markdown", GenerateMarkdown)
+	//http.Handle("/", http.FileServer(http.Dir("public")))
+	//http.ListenAndServe(":"+port, nil)
 
-    r := httprouter.New()
-    //r.GET("/", HomeHandler)
-    r.Handler("GET", "/", http.FileServer(http.Dir("public")))
-    r.HandlerFunc("GET","/markdown",GenerateMarkdown)
-    // Posts collection
-    r.GET("/posts", PostsIndexHandler)
-    r.POST("/posts", PostsCreateHandler)
+	r := httprouter.New()
+	//r.GET("/", HomeHandler)
+	r.Handler("GET", "/", http.FileServer(http.Dir("public")))
+	r.HandlerFunc("GET", "/markdown", GenerateMarkdown)
+	// Posts collection
+	r.GET("/posts", PostsIndexHandler)
+	r.POST("/posts", PostsCreateHandler)
 
-    // Posts singular
-    r.GET("/posts/:id", PostShowHandler)
-    r.PUT("/posts/:id", PostUpdateHandler)
-    r.GET("/posts/:id/edit", PostEditHandler)
+	// Posts singular
+	r.GET("/posts/:id", PostShowHandler)
+	r.PUT("/posts/:id", PostUpdateHandler)
+	r.GET("/posts/:id/edit", PostEditHandler)
 
-    fmt.Println("Starting server on :"+port)
-    http.ListenAndServe(":"+port, nil)
+	fmt.Println("Starting server on :" + port)
+	http.ListenAndServe(":" + port, r)
 }
 
 func GenerateMarkdown(rw http.ResponseWriter, r *http.Request) {
-    markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
-    rw.Write(markdown)
+	markdown := blackfriday.MarkdownCommon([]byte(r.FormValue("body")))
+	rw.Write(markdown)
 }
 
 func HomeHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    fmt.Fprintln(rw, "Home")
+	fmt.Fprintln(rw, "Home")
 }
 
 func PostsIndexHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    fmt.Fprintln(rw, "posts index")
+	fmt.Fprintln(rw, "posts index")
 }
 
 func PostsCreateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    fmt.Fprintln(rw, "posts create")
+	fmt.Fprintln(rw, "posts create")
 }
 
 func PostShowHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    id := p.ByName("id")
-    fmt.Fprintln(rw, "showing post", id)
+	id := p.ByName("id")
+	fmt.Fprintln(rw, "showing post", id)
 }
 
 func PostUpdateHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    fmt.Fprintln(rw, "post update")
+	fmt.Fprintln(rw, "post update")
 }
 
 func PostDeleteHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    fmt.Fprintln(rw, "post delete")
+	fmt.Fprintln(rw, "post delete")
 }
 
 func PostEditHandler(rw http.ResponseWriter, r *http.Request, p httprouter.Params) {
-    fmt.Fprintln(rw, "post edit")
+	fmt.Fprintln(rw, "post edit")
 }
